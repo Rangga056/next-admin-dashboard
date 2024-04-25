3444fa14bbc15ce7df2b1998b1ec212ea13d57e9
Rangga056 <muhammadrangga056@gmail.com>
2024-04-25 12:35:24 +0700

    Setting up database
    -connect to supabase using prisma
    -create prisma schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div>
-      <h1>Next Js Admin Dashboard</h1>
+    <div className="text-red-500">
+      <h1 className="text-red-700">Next Js Admin Dashboard</h1>
+      <h2 className="text-slate-600">Hallo</h2>
+      <p className="text-xl font-semiboild uppercase">This is a next js admin dashboard</p>
     </div>
   );
 }
